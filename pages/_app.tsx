50b25319a3b4bdd3890eaccb7b676b43155e6753
bloodyrainday <bloodyrainday@gmail.com>
2025-06-02 19:23:23 +0300

    create queryClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import type {AppProps} from 'next/app';
-import {ReactElement, ReactNode} from 'react';
-import {NextPage} from 'next';
+import type { AppProps } from "next/app";
+import { ReactElement, ReactNode, useState } from "react";
+import { NextPage } from "next";
+import { QueryClient } from "@tanstack/query-core";
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -11,6 +12,9 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  //create queryClient
+  const [queryClient] = useState(() => new QueryClient());
+
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return getLayout(<Component {...pageProps} />);
