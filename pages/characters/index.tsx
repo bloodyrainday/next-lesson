b6617e5d7764854eec78745828515360611a94bd
bloodyrainday <bloodyrainday@gmail.com>
2025-06-02 14:55:10 +0300

    create getStaticProps in characters page

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,3 +1,22 @@
-const Characters = () => <>Characters</>;
+import { API } from "../../assets/api/api";
+import {
+  CharacterType,
+  ResponseType,
+} from "../../assets/api/rick-and-morty-api";
+
+export const getStaticProps = async () => {
+  const characters = await API.rickAndMorty.getCharacters();
+  return {
+    props: {
+      characters,
+    },
+  };
+};
+
+type PropsType = {
+  characters: ResponseType<CharacterType>;
+};
+
+const Characters = (props: PropsType) => <>Characters</>;
 
 export default Characters;
