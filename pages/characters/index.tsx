91302c57f0251b3271c7c9019e4547a97c578134
bloodyrainday <bloodyrainday@gmail.com>
2025-06-02 16:10:05 +0300

    render characters from api

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -3,6 +3,8 @@ import {
   CharacterType,
   ResponseType,
 } from "../../assets/api/rick-and-morty-api";
+import { Header } from "../../components/Header/Header";
+import { PageWrapper } from "../../components/PageWrapper/PageWrapper";
 
 export const getStaticProps = async () => {
   const characters = await API.rickAndMorty.getCharacters();
@@ -17,6 +19,17 @@ type PropsType = {
   characters: ResponseType<CharacterType>;
 };
 
-const Characters = (props: PropsType) => <>Characters</>;
+const Characters = (props: PropsType) => {
+  const { characters } = props;
+  const charactersList = characters.results.map((c) => (
+    <div key={c.id}>{c.name}</div>
+  ));
+  return (
+    <PageWrapper>
+      <Header />
+      {charactersList}
+    </PageWrapper>
+  );
+};
 
 export default Characters;
