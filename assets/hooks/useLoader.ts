a111ac4891f1426b4a12ab6947573efe3d50ea02
bloodyrainday <bloodyrainday@gmail.com>
2025-06-03 11:57:12 +0300

    complete useLoader hook and use it in _app.tsx

--- assets/hooks/useLoader.ts
+++ assets/hooks/useLoader.ts
@@ -1,9 +1,22 @@
 import { useRouter } from "next/router";
 import nProgress from "nprogress";
+import { useEffect } from "react";
 
 export const useLoader = () => {
   const router = useRouter();
 
-  const startLoading = () => nProgress.start();
-  const endLoading = () => nProgress.done();
+  useEffect(() => {
+    const startLoading = () => nProgress.start();
+    const endLoading = () => nProgress.done();
+
+    router.events.on("routeChangeStart", startLoading);
+    router.events.on("routeChangeComplete", endLoading);
+    router.events.on("routeChangeError", endLoading);
+
+    return () => {
+      router.events.off("routeChangeStart", startLoading);
+      router.events.off("routeChangeComplete", endLoading);
+      router.events.off("routeChangeError", endLoading);
+    };
+  }, [router]);
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ReactElement, ReactNode, useState } from "react";
 import { NextPage } from "next";
 import { QueryClient } from "@tanstack/query-core";
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
+import { useLoader } from "../assets/hooks/useLoader";
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,6 +16,10 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   //create queryClient
   const [queryClient] = useState(() => new QueryClient());
+
+  // add useLoader hook
+
+  useLoader();
 
   const getLayout = Component.getLayout ?? ((page) => page);
 
