eb45eb679e4c85c846b38f45085eb0a265d28da4
bloodyrainday <bloodyrainday@gmail.com>
2025-06-05 17:01:40 +0300

    import CharacterCard component using dynamic import

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,11 +1,15 @@
-import { API } from "../../assets/api/api";
-import {
-  CharacterType,
-  ResponseType,
-} from "../../assets/api/rick-and-morty-api";
-import { CharacterCard } from "../../components/Card/CharacterCard/CharacterCard";
-import { getLayout } from "../../components/Layout/BaseLayout/BaseLayout";
-import { PageWrapper } from "../../components/PageWrapper/PageWrapper";
+import dynamic from "next/dynamic";
+import { API } from "assets/api/api";
+import { CharacterType, ResponseType } from "assets/api/rick-and-morty-api";
+//import { CharacterCard } from "components/Card/CharacterCard/CharacterCard";
+import { getLayout } from "components/Layout/BaseLayout/BaseLayout";
+import { PageWrapper } from "components/PageWrapper/PageWrapper";
+
+const CharacterCard = dynamic(() =>
+  import("components/Card/CharacterCard/CharacterCard").then((module) => {
+    return module.CharacterCard;
+  })
+);
 
 export const getStaticProps = async () => {
   const characters = await API.rickAndMorty.getCharacters();
