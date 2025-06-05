1cb9b28382ff114b18e656426548d6de196cec7d
bloodyrainday <bloodyrainday@gmail.com>
2025-06-05 11:51:58 +0300

    add Cache-Control header in episodes getServerSideProps function

--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -1,10 +1,15 @@
+import { GetServerSideProps } from "next";
 import { API } from "../../assets/api/api";
 import { EpisodeType, ResponseType } from "../../assets/api/rick-and-morty-api";
 import { Card } from "../../components/Card/Card";
 import { getLayout } from "../../components/Layout/BaseLayout/BaseLayout";
 import { PageWrapper } from "../../components/PageWrapper/PageWrapper";
 
-export const getServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=100"
+  );
   const episodes = await API.rickAndMorty.getEpisodes();
 
   if (!episodes) {
